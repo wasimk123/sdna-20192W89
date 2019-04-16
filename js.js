d5fd7f64edaea758a2bb7952409a8274d57ae017
octopusShiva <orjerome@outlook.com>
2019-04-16 11:00:22 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,22 @@
+
+/* functions for nav bar*/
+
 	function clk_home(){
 	  location.replace("home.html")
+	}
+
+	function clk_login(){
+	  location.replace("login.html")
+	}
+
+	function clk_appointment(){
+	  location.replace("set_appointment.html")
+	}
+
+	function clk_tv(){
+	  location.replace("tv.html")
+	}
+
+	function clk_about(){
+	  location.replace("about.html")
 	}
